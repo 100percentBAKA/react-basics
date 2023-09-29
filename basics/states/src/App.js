b36780e6dae4479849f8ee973edee94d6914583c
ad4r5hgs5 <1rn21cs011.adarshgs@gmail.com>
2023-09-29 23:00:03 +0530

    Update App.js

--- basics/states/src/App.js
+++ basics/states/src/App.js
@@ -3,6 +3,9 @@ import { useState } from 'react'
 
 const App = (props) => {
 
+    // ! We can only call a hook from the top level of a Component
+    // ! A react hook cannot be used outside the scope of the Component
+    const [showEvents, setShowEvents] = useState(true)
     const [events, setEvents] = useState([
         { title: 'Study Maths module 1', id: 1 },
         { title: 'Learn React and firebase', id: 2 },
@@ -19,7 +22,14 @@ const App = (props) => {
 
     return (
         <div className='App'>
-            {events.map((event, index) => (
+            <div>
+                {showEvents && (<button onClick={() => (setShowEvents(false))}>Hide</button>)}
+                {!showEvents && (<button onClick={() => (setShowEvents(true))}>Show</button>)}
+            </div>
+
+            {/*showEvents && events.map( ... ) is a javascript expression*/}
+
+            {showEvents && events.map((event, index) => (
                 <div key={event.id}>
                     <h1>{index} - {event.title}</h1>
                     <button onClick={() => (handleClick(event.id))}>Delete</button>
