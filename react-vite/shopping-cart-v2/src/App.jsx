241913526e4f5c42cebae484b224def126ac9dc1
ad4r5hgs5 <1rn21cs011.adarshgs@gmail.com>
2023-10-15 23:11:10 +0530

    ⭐ Initial Commit

---
+++ react-vite/shopping-cart-v2/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl">Hello world</h1>
+      <p className="text-red-400 text-4xl">Create React app bad Vite good</p>
+    </>
+  );
+}
+
+export default App;
